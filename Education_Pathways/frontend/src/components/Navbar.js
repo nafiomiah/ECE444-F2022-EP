4818d0579a2001a0d06d03eb1ab2c55d63f7349b
Nafio Miah <nafiomiah@hotmail.com>
2022-09-19 16:09:40 -0400

    Add new tab to navigation bar

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -10,7 +10,7 @@ import CourseDescriptionPage from "./CourseDescription";
 // import SignUp from './SignUp'
 import SearchResultDisplay from './ResultDisplay'
 
-function CourseDescription (props) {
+function CourseDescription(props) {
   let query = useQuery();
   return <CourseDescriptionPage code={query.get("code")} />;
 }
@@ -24,7 +24,7 @@ function useQuery() {
 
 export default class NavbarComp extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       username: localStorage.getItem('username'),
@@ -34,13 +34,13 @@ export default class NavbarComp extends Component {
 
   componentDidMount() {
     if (localStorage.getItem('username') !== "") {
-      this.setState({username: localStorage.getItem('username')})
+      this.setState({ username: localStorage.getItem('username') })
     }
   }
 
   logOut = () => {
     localStorage.setItem('username', "");
-    this.setState({username: ""})
+    this.setState({ username: "" })
   }
 
   render() {
@@ -66,7 +66,9 @@ export default class NavbarComp extends Component {
                   Search
                 </Nav.Link> */}
 
-                
+                <Nav.Link as={Link} to="/ECE444-Lab1">
+                  ECE444-Lab1
+                </Nav.Link>
 
               </Nav>
             </Navbar.Collapse>
@@ -74,32 +76,35 @@ export default class NavbarComp extends Component {
         </div>
         <div>
           <Switch>
-          <Route path="/about">
-            <div className = "body_text">
-            <p>
+            <Route path="/about">
+              <div className="body_text">
+                <p>
 
-      Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
+                  Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
 
-Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
+                  Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
 
-We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
-</p>
-<p> 
-  <b>Development Team: </b>
-</p>
-<p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
-<p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
+                  We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href="mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
+                </p>
+                <p>
+                  <b>Development Team: </b>
+                </p>
+                <p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
+                <p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
 
 
-      </div>
+              </div>
               {/* <SearchResultDisplay /> */}
+            </Route>
+            <Route path="/ECE444-Lab1">
+              <p> Nafio Miah </p>
             </Route>
             <Route path="/search">
               <SearchResultDisplay />
             </Route>
             <Route exact
               path="/courseDetails/:code"
-              render={props =>(<CourseDescriptionPage {...props} />)}>
+              render={props => (<CourseDescriptionPage {...props} />)}>
             </Route>
             <Route path="/">
               <SearchResultDisplay />
@@ -107,9 +112,9 @@ We are looking for feedback to improve Education Pathways and make it more usefu
 
           </Switch>
         </div>
-        
-        
-    
+
+
+
       </Router>
     );
   }
